43034b8482e438ba5189ff410e417b691bdec72d
LeafDev <leafdev@github.com>
2018-12-26 16:02:51 +0800

    2,1,1 - Add lock software feature

---
+++ _presenters/logoutPresenter.php
@@ -0,0 +1,28 @@
+<?php
+class logoutPresenter {
+    public function get(){
+        SessionModel::destroy();
+
+        header("location: /login");
+        exit;
+    }
+
+    // HTTP Header Method: POST
+    // Usually used when to insert a new data
+    public function post(){
+        Route::returnCode(401);
+    }
+
+    // HTTP Header Method: PUT
+    // Usually used when about to update a data
+    public function put(){
+        Route::returnCode(401);
+    }
+
+    // HTTP Header Method: DELETE
+    // Usually used when about to delete a data
+    public function delete(){
+        Route::returnCode(401);
+    }
+}
+    
